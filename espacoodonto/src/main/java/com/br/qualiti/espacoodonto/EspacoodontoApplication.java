2c77c72ec25b9425bccba36c735ff9ad487682d1
ricopomps <ricardopompiliodossantos@gmail.com>
2020-01-20 00:38:56 -0300

    Projeto criado

---
+++ espacoodonto/src/main/java/com/br/qualiti/espacoodonto/EspacoodontoApplication.java
@@ -0,0 +1,13 @@
+package com.br.qualiti.espacoodonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacoodontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacoodontoApplication.class, args);
+	}
+
+}
---
+++ espacoodonto/src/test/java/com/br/qualiti/espacoodonto/EspacoodontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.qualiti.espacoodonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspacoodontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
